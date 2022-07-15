a4eb10da6906d2459fe0ebc467379c602d78da0d
Toybear1986 <vaskorobogatov@yandex.ru>
2022-07-15 09:22:24 +0300

    Camelcase applied

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        int current_balance = 100;
+        int currentBalance = 100;
         int payment = 1100;
-        int required_payment = 1000;
-        int bonus_peace = 100;
+        int requiredPayment = 1000;
+        int bonusPeace = 100;
         int bonus;
 
-        if (payment >= required_payment) {
-            bonus = payment / bonus_peace;
+        if (payment >= requiredPayment) {
+            bonus = payment / bonusPeace;
         } else {
             bonus = 0;
         }
 
-        int new_balance = current_balance + payment + bonus;
+        int newBalance = currentBalance + payment + bonus;
 
         if (bonus > 0) {
-            System.out.println("Баланс пополнен на: " + payment + " рублей. Вам начислено " + bonus + " бонусных рублей." + " Текущий баланс: " + new_balance + " рублей.");
+            System.out.println("Баланс пополнен на: " + payment + " рублей. Вам начислено " + bonus + " бонусных рублей." + " Текущий баланс: " + newBalance + " рублей.");
         } else {
-            System.out.println("Баланс пополнен на: " + payment + " рублей. Текущий баланс: " + new_balance + " рублей.");
+            System.out.println("Баланс пополнен на: " + payment + " рублей. Текущий баланс: " + newBalance + " рублей.");
         }
 
     }
